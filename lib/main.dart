9d3cf5fb25b7012d3b8e505892b9777767f7e94d
kidsage <ruhyun92@gmail.com>
2023-01-23 17:09:15 +0900

    Test: setState, list 형식 테스트 진행

--- lib/main.dart
+++ lib/main.dart
@@ -12,13 +12,20 @@ class App extends StatefulWidget {
 
 // widget의 state는 widget에 들어갈 데이터와 UI를 넣는 곳
 class _AppState extends State<App> {
-  int counter = 0; // 단순 프로퍼티 not final
+  // int counter = 0; // 단순 프로퍼티 not final
+
+  List<int> numbers = [];
 
   void onClicked() {
     // state 클래스에게 데이터가 변경되었다고 알려주는 함수 (interactive function)
     setState(() {
-      counter = counter + 1;
+      // counter = counter + 1;
+      numbers.add(numbers.length);
+      // print(numbers);
     });
+    // 위의 기능과 동일하지만, 가독성을 위해서 setState안에 기능을 넣는다.
+    // numbers.add(numbers.length);
+    // setState(() {});
   }
 
   @override
@@ -34,10 +41,11 @@ class _AppState extends State<App> {
                 "Click Count",
                 style: TextStyle(fontSize: 30),
               ),
-              Text(
-                '$counter',
-                style: const TextStyle(fontSize: 30),
-              ),
+              // Text(
+              //   '$counter',
+              //   style: const TextStyle(fontSize: 30),
+              // ),
+              for (var n in numbers) Text("$n"),
               IconButton(
                 onPressed: onClicked,
                 icon: const Icon(Icons.add_box_rounded),
