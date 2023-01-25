da8fd89c96a60e4f7a6326d6877a07fbdae655a9
kidsage <ruhyun92@gmail.com>
2023-01-25 18:25:14 +0900

    Test: stetefulwidget widget lifecycle 테스
    - initState, dispose 테스트 완료

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ void main() {
 }
 
 // widget에 데이터를 저장하고 상태가 변환하는 것을 보고 싶을 때 statefulwidget 사용
+// statefulwidget은 initstate > build > dispose 의 lifecycle을 가지고 있다
 class App extends StatefulWidget {
   @override
   State<App> createState() => _AppState();
@@ -12,6 +13,15 @@ class App extends StatefulWidget {
 
 // widget의 state는 widget에 들어갈 데이터와 UI를 넣는 곳
 class _AppState extends State<App> {
+  bool showTitle = true;
+
+  void toggleTitle() {
+    setState(() {
+      // showTitle true면 false되고 반대도 됨
+      showTitle = !showTitle;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -27,8 +37,12 @@ class _AppState extends State<App> {
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
-            children: const [
-              MyLargeTitle(),
+            children: [
+              showTitle ? const MyLargeTitle() : const Text('nothing to see'),
+              IconButton(
+                onPressed: toggleTitle,
+                icon: const Icon(Icons.remove_red_eye),
+              ),
             ],
           ),
         ),
@@ -37,19 +51,43 @@ class _AppState extends State<App> {
   }
 }
 
-class MyLargeTitle extends StatelessWidget {
+class MyLargeTitle extends StatefulWidget {
   const MyLargeTitle({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<MyLargeTitle> createState() => _MyLargeTitleState();
+}
+
+class _MyLargeTitleState extends State<MyLargeTitle> {
+  // 위에서 변수로 이니셜라이징 할 수도 있지만, 부모 요소에 의해 이니셜라이징을 하는 경우에는 아래처럼 사용
+  // 한 번만 호출된다 (build 이전)
+  @override
+  void initState() {
+    super.initState();
+    print('initState');
+  }
+
+  // widget이 screen에서 제거될 때 호출되는 메서드
+  @override
+  void dispose() {
+    super.dispose();
+    print("dispose!");
+  }
+
+  @override
   Widget build(BuildContext context) {
+    print("build!");
     // context는 text의 부모 요소들에 대한 정보가 담겨있다
     return Text(
       "My Large Title",
       style: TextStyle(
         fontSize: 30,
-        color: Theme.of(context).textTheme.titleLarge!.color,
+        color: Theme.of(context)
+            .textTheme
+            .titleLarge!
+            .color, // 상위 클래스의 정보를 가져와서 적용
       ),
     );
   }
